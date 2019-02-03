0bc7dc9c1c4e3ac63a6b52e97c902e9356de5ea1
mofon <yzkf000@foxmail.com>
2019-02-03 20:16:11 +0800

    update wrpc

--- wrpc.js
+++ wrpc.js
@@ -1,19 +1,64 @@
 const UnitSep = "\x1f"
 
 export default {
-	client(url,token) {
+	client(url, token) {
+		let onAuth = () => {}
+		let onIErr = () => {}
+		let onBan = () => {}
+
 		return {
-			call(method,args...) {
-				fetch(url,{
-					method:'POST',
-					body:[method,args...].join(UnitSep),
-					mode: 'cors',
-				}).then(resp=>{
-					let data = resp.text().split(UnitSep)
-					console.log(data)
-				}).catch(e=>{
-					console.log(e)
+			setOnAuth(fn) {
+				onAuth = fn
+			},
+			setOnIErr(fn) {
+				onIErr = fn
+			},
+			setOnBan(fn) {
+				onBan = fn
+			},
+			call(method, ...args) {
+				return new Promise((resolve, reject) => {
+					fetch(url, {
+						method: 'POST',
+						body: [token, method, ...args].join(UnitSep),
+						mode: 'cors',
+					}).then(resp => resp.text())
+						.then(raw => {
+							let data = raw.split(UnitSep)
+							let status = data[0]
+							let rets = data.slice(1)
+
+							let ret = {
+								status,
+								rets
+							}
+							switch (status) {
+								case 'ok':
+									resolve(ret)
+									return
+								case 'auth':
+									onAuth(ret)
+									break
+								case 'ierr':
+									onIErr(ret)
+									break
+								case 'ban':
+									onBan(ret)
+									break
+								default:
+							}
+							reject(ret) // must handle 'err'
+						})
+						.catch(e => {
+							let ret = {
+								status: 'ierr',
+								rets: ["client error", e.message],
+							}
+							onIErr(ret)
+							reject(ret)
+						})
 				})
+
 			}
 		}
 	}
